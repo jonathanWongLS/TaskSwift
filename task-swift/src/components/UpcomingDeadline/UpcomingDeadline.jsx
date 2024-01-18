f758639ac0f1236dafca85e2396a5c65677f0553
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-01-19 05:02:41 +0800

    Fixed dummy values for each upcoming Deadlines details and changed Task icon to Dot icon

--- task-swift/src/components/UpcomingDeadline/UpcomingDeadline.jsx
+++ task-swift/src/components/UpcomingDeadline/UpcomingDeadline.jsx
@@ -1,5 +1,5 @@
 import Card from "react-bootstrap/Card";
-import { GoProjectRoadmap } from "react-icons/go";
+import { RxDot } from "react-icons/rx";
 import "./UpcomingDeadline.css";
 
 const UpcomingDeadline = ({deadlineDetails}) => {
@@ -8,12 +8,12 @@ const UpcomingDeadline = ({deadlineDetails}) => {
         <Card className="upcoming-deadline-card">
             <Card.Title className="p-3 upcoming-deadline-title">Upcoming Deadlines</Card.Title>
             <Card.Body className="upcoming-deadline-body">
-              {[["Task 1", "Project 1", "10 days"], ["Task 2", "Project 2", "2 days"], ["Task 3", "Project 3", "1 day"], ["Task 3", "1 day"], ["Task 3", "1 day"]].map((deadlineDetail, key) => {
+              {[["Task 1", "Project 1", "10 days"], ["Task 2", "Project 2", "2 days"], ["Task 3", "Project 3", "1 day"], ["Task 4", "Project 4", "1 day"], ["Task 5", "Project 5", "1 day"]].map((deadlineDetail, key) => {
                 return (
                   <div key={key} className="upcoming-deadline-task">
                     <div className="upcoming-deadline-task-details-top">
                       <h4>{deadlineDetail[0]}</h4>
-                      <p><GoProjectRoadmap />{deadlineDetail[1]}</p>
+                      <p><RxDot />{deadlineDetail[1]}</p>
                     </div>
                     <div className="upcoming-deadline-task-details-bottom">
                       <p>Remaining Time: <b>{deadlineDetail[2]}</b></p>
