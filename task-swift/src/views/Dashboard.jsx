50fcabb330871b54052974181d7d2a2d89404ded
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-01-19 03:09:09 +0800

    Remove redundant commented code

--- task-swift/src/views/Dashboard.jsx
+++ task-swift/src/views/Dashboard.jsx
@@ -27,57 +27,6 @@ const Dashboard = () => {
                 return <SummaryCard key={index} cardTitle={cardDetails[0]} iconSrc={cardDetails[1]} value={index} darkBg={false} />
               }
             })}
-            {/* <Card className="text-center in-progress">
-              <Card.Body>
-                <Row>
-                  <Col lg={12} xl={4} className="top-dashboard-cards-col">
-                    <div>
-                      <img width={40} height={40} src="progress-bar.svg" />
-                    </div>
-                  </Col>
-                  <Col lg={12} xl={8}>
-                    <Card.Title>In Progress</Card.Title>
-                    <Card.Text>
-                      <h4>3</h4>
-                    </Card.Text>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-            <Card className="text-center completed">
-              <Card.Body>
-                <Row>
-                  <Col lg={12} xl={4} className="top-dashboard-cards-col">
-                    <div className="top-dashboard-cards-img">
-                      <img width={40} height={40} src="task-complete.svg" />
-                    </div>
-                  </Col>
-                  <Col lg={12} xl={8}>
-                    <Card.Title>Completed</Card.Title>
-                    <Card.Text>
-                      <h5>3</h5>
-                    </Card.Text>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-            <Card className="text-center overdue">
-              <Card.Body>
-                <Row>
-                  <Col lg={12} xl={4} className="top-dashboard-cards-col">
-                    <div className="top-dashboard-cards-img">
-                      <img width={40} height={40} src="expire.svg" />
-                    </div>
-                  </Col>
-                  <Col lg={12} xl={8}>
-                    <Card.Title>Overdue</Card.Title>
-                    <Card.Text>
-                      <h5>3</h5>
-                    </Card.Text>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card> */}
           </Col>
         </Row>
       </div>
