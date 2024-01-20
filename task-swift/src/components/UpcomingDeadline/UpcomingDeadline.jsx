778f62b461a3e35f3d6ed6541c9fc9033da101c6
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-01-21 05:27:51 +0800

    Fixed Upcoming Deadlines' Card's scrollable elements pushing component below it

--- task-swift/src/components/UpcomingDeadline/UpcomingDeadline.jsx
+++ task-swift/src/components/UpcomingDeadline/UpcomingDeadline.jsx
@@ -2,31 +2,38 @@ import Card from "react-bootstrap/Card";
 import { RxDot } from "react-icons/rx";
 import "./UpcomingDeadline.css";
 
-const UpcomingDeadline = ({deadlineDetails}) => {
+const UpcomingDeadline = ({ deadlineDetails }) => {
   return (
-    <div className="upcoming-deadline-container">
-        <Card className="upcoming-deadline-card">
-            <Card.Title className="p-3 upcoming-deadline-title">Upcoming Deadlines</Card.Title>
-            <Card.Body className="upcoming-deadline-body">
-              {[["Task 1", "Project 1", "10 days"], ["Task 2", "Project 2", "2 days"], ["Task 3", "Project 3", "1 day"], ["Task 4", "Project 4", "1 day"], ["Task 5", "Project 5", "1 day"]].map((deadlineDetail, key) => {
-                return (
-                  <div key={key} className="upcoming-deadline-task">
-                    <div className="upcoming-deadline-task-details-top">
-                      <h4>{deadlineDetail[0]}</h4>
-                      <RxDot />
-                      {deadlineDetail[1]}
-                    </div>
-                    <div className="upcoming-deadline-task-details-bottom">
-                      <p>Remaining Time: <b>{deadlineDetail[2]}</b></p>
-                    </div>
+    <Card className="upcoming-deadline-card" style={{ height: '400px', overflowY: 'auto' }}>
+      <Card.Body className="upcoming-deadline-card-body">
+        <Card.Title className="upcoming-deadline-card-title">Upcoming Deadlines</Card.Title>
+        <Card.Text>
+          {[
+              ["Task 1", "Project 1", "10 days"],
+              ["Task 2", "Project 2", "2 days"],
+              ["Task 3", "Project 3", "1 day"],
+              ["Task 4", "Project 4", "1 day"],
+              ["Task 5", "Project 5", "1 day"],
+            ].map((deadlineDetail, key) => {
+              return (
+                <div key={key} className="upcoming-deadline-task">
+                  <div className="upcoming-deadline-task-details-top">
+                    <h4>{deadlineDetail[0]}</h4>
+                    <RxDot />
+                    {deadlineDetail[1]}
                   </div>
-                );
-              })}
-              
-            </Card.Body>
-        </Card>
-    </div>
-  )
-}
+                  <div className="upcoming-deadline-task-details-bottom">
+                    <p>
+                      Remaining Time: <b>{deadlineDetail[2]}</b>
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
 
-export default UpcomingDeadline
+export default UpcomingDeadline;
