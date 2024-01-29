3026d45f316657e1d1a62f191df21b7bd79d8b34
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-01-29 17:14:06 +0800

    Implemented modal form for Add Task feature

--- task-swift/src/components/TaskList/TaskList.jsx
+++ task-swift/src/components/TaskList/TaskList.jsx
@@ -197,117 +197,117 @@ const TaskList = () => {
                 </Button>
 
                 <Modal dialogClassName="modal-add-task" show={showAddTaskModal} onHide={handleCloseAddTaskModal}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Add New Task</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Row>
-                        <Col sm={12} md={12} xl={8}>
-                            <Form>
-                                <Form.Group className="mb-3 task-name-group">
-                                    <Form.Label>
-                                        <h5>Name</h5> 
-                                    </Form.Label>
-                                    <Form.Control type="text" />
-                                    <Form.Text muted>Name of the task</Form.Text>
-                                </Form.Group>
-                                <Form.Group className="mb-3 task-desc-group">
-                                    <Form.Label>
-                                        <h5>Description</h5>
-                                    </Form.Label>
-                                    <Form.Control as="textarea" rows={3} />
-                                    <Form.Text muted>Description of the task</Form.Text>
-                                </Form.Group>
-                                <div className="task-timeframe-container">
-                                    <Form.Group className="mb-3 task-timeframe-group">
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add New Task</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Row>
+                            <Col sm={12} md={12} xl={8}>
+                                <Form>
+                                    <Form.Group className="mb-3 task-name-group">
                                         <Form.Label>
-                                            <h5>Timeframe</h5>
+                                            <h5>Name</h5> 
                                         </Form.Label>
-                                        <div className="task-timeframe-inputs-container">
-                                            <InputGroup className="task-timeframe-start-group">
-                                                <Form.Control placeholder="Starts at (mm/dd/yyyy)" />
-                                                <Button variant="outline-secondary">
-                                                    X
-                                                </Button>
-                                            </InputGroup>
-                                            <BsDashLg />
-                                            <InputGroup className="task-timeframe-end-group">
-                                                <Form.Control placeholder="Ends at (mm/dd/yyyy)" />
-                                                <Button variant="outline-secondary" id="button-addon2">
-                                                X
-                                                </Button>
-                                            </InputGroup>
-                                        </div>
+                                        <Form.Control type="text" />
+                                        <Form.Text muted>Name of the task</Form.Text>
                                     </Form.Group>
-                                </div> 
+                                    <Form.Group className="mb-3 task-desc-group">
+                                        <Form.Label>
+                                            <h5>Description</h5>
+                                        </Form.Label>
+                                        <Form.Control as="textarea" rows={3} />
+                                        <Form.Text muted>Description of the task</Form.Text>
+                                    </Form.Group>
+                                    <div className="task-timeframe-container">
+                                        <Form.Group className="mb-3 task-timeframe-group">
+                                            <Form.Label>
+                                                <h5>Timeframe</h5>
+                                            </Form.Label>
+                                            <div className="task-timeframe-inputs-container">
+                                                <InputGroup className="task-timeframe-start-group">
+                                                    <Form.Control placeholder="Starts at (mm/dd/yyyy)" />
+                                                    <Button variant="outline-secondary">
+                                                        X
+                                                    </Button>
+                                                </InputGroup>
+                                                <BsDashLg />
+                                                <InputGroup className="task-timeframe-end-group">
+                                                    <Form.Control placeholder="Ends at (mm/dd/yyyy)" />
+                                                    <Button variant="outline-secondary" id="button-addon2">
+                                                    X
+                                                    </Button>
+                                                </InputGroup>
+                                            </div>
+                                        </Form.Group>
+                                    </div> 
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>
+                                            <h5>Priority</h5>
+                                        </Form.Label>
+                                        <Dropdown onSelect={handleTaskPrioritySelectAdd}>
+                                            <Dropdown.Toggle variant="success" style={getPriorityDropdownColorAdd()}>
+                                                { selectedTaskPriorityAdd }
+                                            </Dropdown.Toggle>
+
+                                            <Dropdown.Menu>
+                                                <div className="low-item-container">
+                                                    <Dropdown.Item eventKey="Low" className="low-item">
+                                                        Low
+                                                    </Dropdown.Item>
+                                                </div>
+                                                <div className="medium-item-container">
+                                                    <Dropdown.Item eventKey="Medium" className="medium-item">
+                                                        Medium
+                                                    </Dropdown.Item>
+                                                </div>
+                                                <div className="high-item-container">
+                                                    <Dropdown.Item eventKey="High" className="high-item">
+                                                        High
+                                                    </Dropdown.Item>
+                                                </div>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                        <Form.Text muted>How important this task is</Form.Text>
+                                    </Form.Group>
+                                </Form>
+                            </Col>
+                            <Col sm={12} md={12} xl={4}>
                                 <Form.Group className="mb-3">
                                     <Form.Label>
-                                        <h5>Priority</h5>
+                                        <h5>Assignees</h5>
+                                        <Form.Check
+                                            type="checkbox"
+                                            label="User 1"    
+                                        />
+                                        <Form.Check
+                                            type="checkbox"
+                                            label="User 1"    
+                                        />
+                                        <Form.Check
+                                            type="checkbox"
+                                            label="User 1"    
+                                        />
+                                        <Form.Check
+                                            type="checkbox"
+                                            label="User 1"    
+                                        />
+                                        <Form.Check
+                                            type="checkbox"
+                                            label="User 1"    
+                                        />
                                     </Form.Label>
-                                    <Dropdown onSelect={handleTaskPrioritySelectAdd}>
-                                        <Dropdown.Toggle variant="success" style={getPriorityDropdownColorAdd()}>
-                                            { selectedTaskPriorityAdd }
-                                        </Dropdown.Toggle>
-
-                                        <Dropdown.Menu>
-                                            <div className="low-item-container">
-                                                <Dropdown.Item eventKey="Low" className="low-item">
-                                                    Low
-                                                </Dropdown.Item>
-                                            </div>
-                                            <div className="medium-item-container">
-                                                <Dropdown.Item eventKey="Medium" className="medium-item">
-                                                    Medium
-                                                </Dropdown.Item>
-                                            </div>
-                                            <div className="high-item-container">
-                                                <Dropdown.Item eventKey="High" className="high-item">
-                                                    High
-                                                </Dropdown.Item>
-                                            </div>
-                                        </Dropdown.Menu>
-                                    </Dropdown>
-                                    <Form.Text muted>How important this task is</Form.Text>
                                 </Form.Group>
-                            </Form>
-                        </Col>
-                        <Col sm={12} md={12} xl={4}>
-                            <Form.Group className="mb-3">
-                                <Form.Label>
-                                    <h5>Assignees</h5>
-                                    <Form.Check
-                                        type="checkbox"
-                                        label="User 1"    
-                                    />
-                                    <Form.Check
-                                        type="checkbox"
-                                        label="User 1"    
-                                    />
-                                    <Form.Check
-                                        type="checkbox"
-                                        label="User 1"    
-                                    />
-                                    <Form.Check
-                                        type="checkbox"
-                                        label="User 1"    
-                                    />
-                                    <Form.Check
-                                        type="checkbox"
-                                        label="User 1"    
-                                    />
-                                </Form.Label>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={handleCloseAddTaskModal}>
-                    Save
-                    </Button>
-                    <Button variant="danger" onClick={handleCloseAddTaskModal}>
-                    Cancel
-                    </Button>
-                </Modal.Footer>
+                            </Col>
+                        </Row>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={handleCloseAddTaskModal}>
+                        Save
+                        </Button>
+                        <Button variant="danger" onClick={handleCloseAddTaskModal}>
+                        Cancel
+                        </Button>
+                    </Modal.Footer>
                 </Modal>
             </>
         </div>
