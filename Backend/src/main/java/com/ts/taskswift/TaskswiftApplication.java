16b94877aee82b6a453a03bc6c54b0361e4cb263
JonathanWongLS <jonathanwongls01@gmail.com>
2024-02-13 17:33:09 +0800

    Added starting backend Java Spring Boot code

---
+++ Backend/src/main/java/com/ts/taskswift/TaskswiftApplication.java
@@ -0,0 +1,13 @@
+package com.ts.taskswift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskswiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskswiftApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ts/taskswift/TaskswiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.taskswift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskswiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
