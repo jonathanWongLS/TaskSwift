7aa318e6827fbd29d5ba950716406c4d99781a53
JonathanWongLS <jonathanwongls01@gmail.com>
2024-02-14 23:00:02 +0800

    Remove typo: Token '[' accidentally added

--- Backend/src/main/java/com/ts/taskswift/service/UserDetailsServiceImpl.java
+++ Backend/src/main/java/com/ts/taskswift/service/UserDetailsServiceImpl.java
@@ -1,6 +1,6 @@
 package com.ts.taskswift.service;
 
-]import com.ts.taskswift.repository.UserRepository;
+import com.ts.taskswift.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
