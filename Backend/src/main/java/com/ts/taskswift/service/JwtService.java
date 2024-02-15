af2986ac9785c72f6e39e50a4982b6866052babf
JonathanWongLS <jonathanwongls01@gmail.com>
2024-02-15 20:26:53 +0800

    Switched second parameter of isValid function to type of UserDetails instead of User

--- Backend/src/main/java/com/ts/taskswift/service/JwtService.java
+++ Backend/src/main/java/com/ts/taskswift/service/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
@@ -17,7 +18,7 @@ public class JwtService {
         return extractClaim(token, Claims::getSubject);
     }
 
-    public boolean isValid(String token, User user) {
+    public boolean isValid(String token, UserDetails user) {
         String username = extractUsername(token);
         return (username.equals(user.getUsername())) && !isTokenExpired(token);
     }
