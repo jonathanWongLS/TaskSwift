9a933b004e5cefbc5a1e170027e890fe1c6b0f8d
JonathanWongLS <jonathanwongls01@gmail.com>
2024-02-15 22:49:53 +0800

    Implemented service functions for authentication purposes including: register and authenticate for registration and login purposes respectively

---
+++ Backend/src/main/java/com/ts/taskswift/service/AuthenticationService.java
@@ -0,0 +1,53 @@
+package com.ts.taskswift.service;
+
+import com.ts.taskswift.model.AuthenticationResponse;
+import com.ts.taskswift.model.User;
+import com.ts.taskswift.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(User request) throws Exception {
+        if (request.getUsername() == null || request.getEmail() == null || request.getPassword() == null || request.getRole() == null) {
+            throw new Exception("One or more register values not found in JSON request!");
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(request.getRole());
+        user = userRepository.save(user);
+        String token = jwtService.generateToken(user);
+        return new AuthenticationResponse(token);
+    }
+
+    public AuthenticationResponse authenticate(User request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+        User user = userRepository.findByUsername(request.getUsername()).orElseThrow(() -> new UsernameNotFoundException("Username not found while authenticating!"));
+        if (passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            String token = jwtService.generateToken(user);
+            return new AuthenticationResponse(token);
+        }
+        else {
+            throw new BadCredentialsException("Password does not match!");
+        }
+    }
+}
