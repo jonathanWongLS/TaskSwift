985bcc11d870adca202d32068c87fa9abfd418d6
JonathanWongLS <jonathanwongls01@gmail.com>
2024-02-15 20:11:31 +0800

    Implemented service functions for Json web tokens (JWTs)

---
+++ Backend/src/main/java/com/ts/taskswift/service/JwtService.java
@@ -0,0 +1,61 @@
+package com.ts.taskswift.service;
+
+import com.ts.taskswift.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+public class JwtService {
+    private final String SECRET_KEY = "04a8e45e2eaaf36f52ac2490530a44f115a66bbeb2acd451ef5f596ebe16dcb8";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isValid(String token, User user) {
+        String username = extractUsername(token);
+        return (username.equals(user.getUsername())) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String generateToken(User user) {
+         return Jwts
+                .builder()
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 24*60*60*1000))
+                .signWith(getSigninKey())
+                .compact();
+    }
+
+    public SecretKey getSigninKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
