90fc696dd9c30b956107cf3768c3b3265bb34caf
JonathanWongLS <jonathanwongls01@gmail.com>
2024-02-16 20:14:17 +0800

    Removed unused import

--- Backend/src/main/java/com/ts/taskswift/service/UserDetailsServiceImpl.java
+++ Backend/src/main/java/com/ts/taskswift/service/UserDetailsServiceImpl.java
@@ -5,7 +5,6 @@ import com.ts.taskswift.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
