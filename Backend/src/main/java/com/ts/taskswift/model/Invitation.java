a145e578a3a08c043f9770c05b2b7320b5829c96
JonathanWongLS <jonathanwongls01@gmail.com>
2024-03-14 02:41:14 +0800

    Created entity for invitation object where each project can have 0 or more invitations

---
+++ Backend/src/main/java/com/ts/taskswift/model/Invitation.java
@@ -0,0 +1,28 @@
+package com.ts.taskswift.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "invitation")
+@Getter
+@Setter
+public class Invitation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "invitation_id")
+    private Long id;
+
+    @Column(name = "invitee_email")
+    private String inviteeEmail;
+
+    @Column(name = "token")
+    private UUID invitationToken;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+}
