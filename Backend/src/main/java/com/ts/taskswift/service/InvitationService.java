429a4295a783fc6ef8faea47ba6a881777e22cda
JonathanWongLS <jonathanwongls01@gmail.com>
2024-03-14 02:50:30 +0800

    Implement InvitationService for Project Invitations

---
+++ Backend/src/main/java/com/ts/taskswift/service/InvitationService.java
@@ -0,0 +1,58 @@
+package com.ts.taskswift.service;
+
+import com.ts.taskswift.exception.ResourceNotFoundException;
+import com.ts.taskswift.model.AuthenticationResponse;
+import com.ts.taskswift.model.Invitation;
+import com.ts.taskswift.model.Project;
+import com.ts.taskswift.model.User;
+import com.ts.taskswift.repository.InvitationRepository;
+import com.ts.taskswift.repository.ProjectRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class InvitationService {
+    private final InvitationRepository invitationRepository;
+    private final AuthenticationService authenticationService;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final ProjectRepository projectRepository;
+    private final ProjectService projectService;
+
+    public InvitationService(InvitationRepository invitationRepository, AuthenticationService authenticationService, UserDetailsServiceImpl userDetailsService, ProjectRepository projectRepository, @Lazy ProjectService projectService) {
+        this.invitationRepository = invitationRepository;
+        this.authenticationService = authenticationService;
+        this.userDetailsService = userDetailsService;
+        this.projectRepository = projectRepository;
+        this.projectService = projectService;
+    }
+
+    public Invitation createInvitationToProject(Long projectId, String email) {
+        Invitation invitation = new Invitation();
+        Project project = projectService.getProjectById(projectId);
+        UUID inviteToken = UUID.randomUUID();
+
+        invitation.setInviteeEmail(email);
+        invitation.setProject(project);
+        invitation.setInvitationToken(inviteToken);
+
+        return invitationRepository.save(invitation);
+    }
+
+    public Invitation loadInvitationByToken(UUID token) {
+        return invitationRepository.findByInvitationToken(token).orElseThrow(() -> new ResourceNotFoundException("Invitation with token " + token + " does not exist. User is not registered"));
+    }
+
+    public AuthenticationResponse registerUserAndAssignProject(Invitation invitation, User user) {
+        AuthenticationResponse registrationToken = authenticationService.register(user);
+        User registeredUser = (User) userDetailsService.loadUserByUsername(user.getUsername());
+        Project project = invitation.getProject();
+        Set<User> assignedUsers = project.getAssignedUsers();
+        assignedUsers.add(registeredUser);
+        project.setAssignedUsers(assignedUsers);
+        projectRepository.save(project);
+        return new AuthenticationResponse(registrationToken.getToken());
+    }
+}
