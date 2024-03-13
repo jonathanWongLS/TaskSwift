ad9818a3115d60938b32c488e57d87b39c397c23
JonathanWongLS <jonathanwongls01@gmail.com>
2024-03-13 22:17:28 +0800

    Added exception constructor with message and cause parameter instead of only message parameter

--- Backend/src/main/java/com/ts/taskswift/exception/ResourceNotFoundException.java
+++ Backend/src/main/java/com/ts/taskswift/exception/ResourceNotFoundException.java
@@ -6,4 +6,8 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
