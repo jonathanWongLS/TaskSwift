a4692440e5ff0987a0f92c7872d869b2baebc7b6
JonathanWongLS <jonathanwongls01@gmail.com>
2024-03-21 05:46:34 +0800

    Updated details to be included in email sent to invited email addresses

--- Backend/src/main/java/com/ts/taskswift/service/EmailService.java
+++ Backend/src/main/java/com/ts/taskswift/service/EmailService.java
@@ -1,34 +1,25 @@
 package com.ts.taskswift.service;
 
-import com.ts.taskswift.email.EmailSender;
 import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
-import org.slf4j.Logger;
 
 @Service
 @AllArgsConstructor
-public class EmailService implements EmailSender {
-    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+public class EmailService {
+    @Autowired
     private final JavaMailSender javaMailSender;
 
-    @Override
-    public void send(String to, String emailContent) {
-        try {
-            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
-            helper.setText(emailContent, true);
-            helper.setTo(to);
-            helper.setFrom("wonglsjonathan01@gmail.com");
-            helper.setSubject("You have been invited to a project!");
-        } catch (MessagingException e) {
-            LOGGER.error("Failed to send email", e);
-            throw new IllegalStateException("Failed to send email!");
-        }
+    public void send(String to, String body) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        message.setFrom(new InternetAddress("wonglsjonathan01@gmail.com"));
+        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(to));
+        message.setSubject("[TaskSwift] You have been invited to a project!");
+        message.setContent(body, "text/html; charset=utf-8");
+        javaMailSender.send(message);
     }
 }
