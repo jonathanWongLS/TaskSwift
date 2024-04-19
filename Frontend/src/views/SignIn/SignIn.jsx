9c8b0a46a0033f0a0b8719af647c3588e5292d9c
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-04-19 22:15:39 +0800

    Modified SignIn which now allows Axios POST call to API for signing in user into the database and including loading animation on Submit button

--- Frontend/src/views/SignIn/SignIn.jsx
+++ Frontend/src/views/SignIn/SignIn.jsx
@@ -1,9 +1,24 @@
-import Header from "../../components/Header/Header";
-import { Form } from "react-bootstrap";
-import "./SignIn.css";
 import { useState } from "react";
 
+import Form from "react-bootstrap/Form";
+import Spinner from "react-bootstrap/Spinner";
+
+import Header from "../../components/Header/Header";
+import AlertBox from "../../components/AlertBox/AlertBox";
+
+import "./SignIn.css";
+
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useJwt } from "react-jwt";
+
 const SignIn = () => {
+  const { decodedToken, isExpired } = useJwt(Cookies.get("jwt"));
+  const [credentialsExist, setCredentialsExist] = useState(true);
+
+  const [signInLoading, setSignInLoading] = useState(false);
+  const [signInError, setSignInError] = useState(null);
+  
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
@@ -15,9 +30,9 @@ const SignIn = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleEmailChange = (e) => {
+  const handleUsernameChange = (e) => {
     e.preventDefault();
-    setFormData({ ...formData, email: e.target.value });
+    setFormData({ ...formData, username: e.target.value });
     console.log(formData);
   };
 
@@ -30,52 +45,95 @@ const SignIn = () => {
   const validatePassword = () => {
     let errors = {};
 
-    if (!formData.email) {
-      errors.email = "Email is required";
+    if (!formData.username) {
+      errors.username = "Username is required";
     } 
-    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formData.email)) {
-      errors.email = "Invalid Email Address!";
-    }
 
     if (!formData.password) {
       errors.password = "Password is required";
     } 
-    else if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/.test(formData.password)) {
-      errors.password = "Password needs to be 8 or more characters long, has at least on uppercase and a lowercase letter, and at least one digit "
-    }
 
     return errors;
   };
 
   const handleSignin = (e) => {
+    setSignInLoading(true);
+
     e.preventDefault();
     var errors = {};
+    setErrors({});
+    setCredentialsExist(true);
     errors = validatePassword(formData.password);
     if (Object.keys(errors).length === 0) {
-        // TODO: If nothing is wrong, proceed with steps with email and password obtained!
+      axios.post(
+        "http://localhost:8081/api/v1/login",
+        {
+          "username": formData.username,
+          "email": "",
+          "password": formData.password,
+          "role": null
+        },
+        {
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+        })
+        .then(function (response) {
+          Cookies.set("jwt", response.data.token);
+          setCredentialsExist(true);
+          console.log(response.data);
+          window.location.href = "/dashboard";
+        })
+        .catch(function (error) {
+          if (error.response) {
+            // The server responded with a status code outside the 2xx range
+            console.log('Error response:', error.response + ". Try again or contact TaskSwift at noreply.taskswift@gmail.com.");
+            if (error.response.status == 401) {
+              window.location.href = "/sign-in?expired=true";
+            } else if (error.response.status == 409) {
+              setCredentialsExist(false);
+            } else {
+              setSignInError(error.response.data + ". Try again or contact TaskSwift at noreply.taskswift@gmail.com.");
+            }
+          } else if (error.request) {
+            // The request was made but no response was received
+            console.log('Error request:', error.message + ". Try again or contact TaskSwift at noreply.taskswift@gmail.com.");
+            setSignInError(error.message + ". Try again or contact TaskSwift at noreply.taskswift@gmail.com.");
+          } else {
+            // Something happened in setting up the request that triggered an error
+            console.log('Error message:', error.message + ". Try again or contact TaskSwift at noreply.taskswift@gmail.com.");
+            setSignInError(error.message + ". Try again or contact TaskSwift at noreply.taskswift@gmail.com.");
+          }
+          setTimeout(() => {
+            setSignInError(null);
+          }, 6000);
+        })
+        .finally(() => setSignInLoading(false));
     }
     else {
-        setErrors(errors);
+      setErrors(errors);
     }
-
   }
 
   return (
     <>
-      <Header />
+      { decodedToken ? <Header loggedIn={true} username={decodedToken.sub} /> : <Header loggedIn={false} username={null} /> }
       <div className="Auth-form-container">
+       
+       <AlertBox errorMessage={ signInError } />
+
         <form noValidate className="Auth-form" onSubmit={handleSignin}>
           <div className="Auth-form-content">
             <h3 className="Auth-form-title">Sign in</h3>
-            <div className="form-group email mt-3">
+            <div className="form-group username mt-3">
               <input
-                type="email"
+                type="text"
                 className="form-control mt-1"
-                placeholder="Email"
-                value={formData.email}
-                onChange={handleEmailChange}
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleUsernameChange}
               />
-              <p className="error">{errors.email}</p>
+              <p className="error">{errors.username}</p>
             </div>
             <div className="form-group password mt-3 position-relative">
               <input
@@ -100,12 +158,21 @@ const SignIn = () => {
             <div className="d-grid gap-2 mt-3">
               <button type="submit" className="btn btn-primary submit-btn">
                 Submit
+                {
+                  signInLoading ? 
+                  <span className="d-flex justify-content-center text-align-center">
+                    <Spinner animation="grow" size="sm" />
+                  </span>
+                  :
+                  null 
+                }
               </button>
             </div>
             <p className="forgot-password text-right mt-2">
                 <a href="/sign-up">Don&apos;t have an account?</a>
             </p>
           </div>
+          <p className={`error d-flex justify-content-center ${credentialsExist === true ? "d-none" : ""}`}>Username or password does not exist.</p>
         </form>
       </div>
     </>
