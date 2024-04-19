85b2d0c97233ad2222a470aec1ce6f7c9d9482de
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-04-20 00:24:58 +0800

    Removes all old tokens assigned to a previously logged-out user and saves newly assigned token to the logged-in user

--- Backend/src/main/java/com/ts/taskswift/service/AuthenticationService.java
+++ Backend/src/main/java/com/ts/taskswift/service/AuthenticationService.java
@@ -3,7 +3,9 @@ package com.ts.taskswift.service;
 import com.ts.taskswift.exception.UsernameAlreadyExistsException;
 import com.ts.taskswift.exception.PasswordNotFoundException;
 import com.ts.taskswift.model.AuthenticationResponse;
+import com.ts.taskswift.model.Token;
 import com.ts.taskswift.model.User;
+import com.ts.taskswift.repository.TokenRepository;
 import com.ts.taskswift.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +14,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
@@ -19,6 +23,7 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final TokenRepository tokenRepository;
 
     public AuthenticationResponse register(User request) {
         if (request.getUsername() == null || request.getEmail() == null || request.getPassword() == null || request.getRole() == null) {
@@ -51,10 +56,26 @@ public class AuthenticationService {
         User user = userRepository.findByUsername(request.getUsername()).orElseThrow(() -> new UsernameNotFoundException("Username not found while authenticating!"));
         if (passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             String token = jwtService.generateToken(user);
+            revokeAllTokenByUser(user);
+            saveUserToken(user, token);
             return new AuthenticationResponse(token);
         }
         else {
             throw new PasswordNotFoundException("Password does not match!");
+        }
+    }
+
+    private void saveUserToken(User user, String token) {
+        Token tokenToSave = new Token();
+        tokenToSave.setToken(token);
+        tokenToSave.setUser(user);
+        tokenRepository.save(tokenToSave);
+    }
+
+    private void revokeAllTokenByUser(User user) {
+        List<Token> validTokenListByUser = tokenRepository.findAllTokenByUser(user.getId());
+        if (!validTokenListByUser.isEmpty()) {
+            validTokenListByUser.forEach(tokenRepository::delete);
         }
     }
 }
