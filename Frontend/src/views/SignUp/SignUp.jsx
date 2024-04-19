99845b60ea779664e6b242d3dcda693af670bf92
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-04-19 22:17:02 +0800

    Modified SignIn which now allows Axios POST call to API for signing in user into the database, including loading animation on Submit button, and option to sign up with invitation token passed in as a URL parameter

--- Frontend/src/views/SignUp/SignUp.jsx
+++ Frontend/src/views/SignUp/SignUp.jsx
@@ -1,36 +1,48 @@
 import Header from "../../components/Header/Header";
 import { useState } from "react";
-import { Form } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Spinner from "react-bootstrap/Spinner";
+import { useSearchParams } from "react-router-dom";
+
+import "./SignUp.css";
+
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useJwt } from "react-jwt";
 
 const SignUp = () => {
+  const { decodedToken, isExpired } = useJwt(Cookies.get("jwt"));
+  let [searchParams, setSearchParams] = useSearchParams();
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
+    username: "",
     email: "",
     password: "",
     repeatPassword: "",
   });
-  const [errors, setErrors] = useState({});
+  const [formErrors, setFormErrors] = useState({});
+  const [fetchData, setFetchData] = useState(null);
+  const [signUpLoading, setSignUpLoading] = useState(null);
+  const [signUpError, setSignUpError] = useState(null);
 
   const handleShowPassword = () => {
     setShowPassword(!showPassword);
   };
 
+  const handleUsernameChange = (e) => {
+    setFormData({...formData, username: e.target.value});
+  }
+
   const handleEmailChange = (e) => {
-    e.preventDefault();
     setFormData({ ...formData, email: e.target.value });
-    console.log(formData);
   };
 
   const handlePasswordChange = (e) => {
-    e.preventDefault();
     setFormData({ ...formData, password: e.target.value });
-    console.log(formData);
   };
 
   const handleRepeatPasswordChange = (e) => {
-    e.preventDefault();
     setFormData({ ...formData, repeatPassword: e.target.value });
-    console.log(formData);
   };
 
   const validatePassword = () => {
@@ -63,26 +75,129 @@ const SignUp = () => {
     return errors;
   };
 
-  const handleSignup = (e) => {
-    e.preventDefault();
+  const handleSignup = () => {
     var errors = {};
+    setSignUpError(null);
+    setFormErrors({});
     errors = validatePassword(formData.password);
-    console.log(errors);
-    if (Object.keys(errors).length === 0) {
-      // TODO: If nothing is wrong, proceed with steps with email and password obtained!
-    } else {
-      setErrors(errors);
-    }
 
+    if (Object.keys(errors).length === 0) {
+      setSignUpLoading(true);
+      setFetchData(null);
+      setSignUpError(null);
+
+      let token = searchParams.get("token");
+
+      if (token) {
+        axios.post(
+          `http://localhost:8081/api/v1/invite?token=${token}`, 
+          {
+            "username": formData.username,
+            "email": formData.email,
+            "password": formData.password,
+            "role": "USER"
+          },
+          {
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+          })
+          .then((response) => {
+            setSignUpLoading(false);
+            console.log(response);
+            if (response.data) {
+              setFetchData(response.data); 
+              Cookies.set("jwt", response.data.token);
+            
+              sessionStorage.removeItem("username");
+              sessionStorage.removeItem("role");
+  
+              sessionStorage.setItem("username", formData.username);
+              sessionStorage.setItem("role", "USER");
+  
+              window.location.href = "/dashboard";
+            }
+          })
+          .catch((error) => {
+            setSignUpLoading(false);
+            console.log(error);
+            if (error.response) {
+              setSignUpError(error.response.data);
+            } else {
+              setSignUpError("An error occurred. Please reload this page.")  
+            }
+          }
+        );
+      } else {
+        axios.post(
+          "http://localhost:8081/api/v1/register", 
+          {
+            "username": formData.username,
+            "email": formData.email,
+            "password": formData.password,
+            "role": "USER"
+          },
+          {
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+          })
+          .then((response) => {
+            setSignUpLoading(false);
+            console.log(response);
+            if (response.data) {
+              setFetchData(response.data); 
+              Cookies.set("jwt", response.data.token);
+            
+              sessionStorage.removeItem("username");
+              sessionStorage.removeItem("role");
+  
+              sessionStorage.setItem("username", formData.username);
+              sessionStorage.setItem("role", "USER");
+  
+              window.location.href = "/dashboard";
+            }
+          })
+          .catch((error) => {
+            setSignUpLoading(false);
+            console.log(error);
+            if (error.response) {
+              setSignUpError(error.response.data);
+            } else {
+              setSignUpError("An error occurred. Please reload this page.")  
+            }
+          }
+        );
+      }
+    } else {
+      setFormErrors(errors);
+    }
   };
 
   return (
     <>
-      <Header />
+      { decodedToken ? <Header loggedIn={true} username={decodedToken.sub} /> : <Header loggedIn={false} username={null} /> }
       <div className="Auth-form-container">
+        { signUpLoading ? 
+          <span className="d-flex justify-content-center text-align-center">
+            <Spinner animation="grow" size="sm" />
+          </span> 
+          :
+          null
+        }
         <form noValidate className="Auth-form" onSubmit={handleSignup}>
           <div className="Auth-form-content">
             <h3 className="Auth-form-title">Sign up</h3>
+            <div className="form-group username mt-3">
+              <input
+                type="text"
+                className="form-control mt-1"
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleUsernameChange}
+              />
+              <p className="error">{formErrors.username}</p>
+            </div>
             <div className="form-group email mt-3">
               <input
                 type="email"
@@ -91,7 +206,7 @@ const SignUp = () => {
                 value={formData.email}
                 onChange={handleEmailChange}
               />
-              <p className="error">{errors.email}</p>
+              <p className="error">{formErrors.email}</p>
             </div>
             <div className="form-group password mt-3 position-relative">
               <input
@@ -101,7 +216,7 @@ const SignUp = () => {
                 value={formData.password}
                 onChange={handlePasswordChange}
               />
-              <p className="error">{errors.password}</p>
+              <p className="error">{formErrors.password}</p>
             </div>
             <div className="form-group password mt-3 position-relative">
               <input
@@ -111,7 +226,7 @@ const SignUp = () => {
                 value={formData.repeatPassword}
                 onChange={handleRepeatPasswordChange}
               />
-              <p className="error">{errors.repeatPassword}</p>
+              <p className="error">{formErrors.repeatPassword}</p>
               <div className="eye-icon-container">
                 <Form.Group>
                   <Form.Check
@@ -126,12 +241,21 @@ const SignUp = () => {
             <div className="d-grid gap-2 mt-3">
               <button type="submit" className="btn btn-primary submit-btn">
                 Submit
+                {
+                  signUpLoading ? 
+                  <span className="d-flex justify-content-center text-align-center">
+                    <Spinner animation="grow" size="sm" />
+                  </span> 
+                  :
+                  null
+                }
               </button>
             </div>
             <p className="forgot-password text-right mt-2">
               <a href="/sign-in">Already have an account?</a>
             </p>
           </div>
+          { signUpError ? <p className="error d-flex justify-content-center">{signUpError}</p> : null }
         </form>
       </div>
     </>
