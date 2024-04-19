61e4d524710ebc7914c1adc2056bfc8065834cbd
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-04-19 21:12:15 +0800

    Modified UpcomingDeadline component on Dashboard to display upcoming tasks with deadlines at most 7 days from current date (ordered from closest to latest)

--- Frontend/src/components/UpcomingDeadline/UpcomingDeadline.jsx
+++ Frontend/src/components/UpcomingDeadline/UpcomingDeadline.jsx
@@ -1,39 +1,55 @@
 import Card from "react-bootstrap/Card";
 import { RxDot } from "react-icons/rx";
 import "./UpcomingDeadline.css";
+import PropTypes from "prop-types";
 
 const UpcomingDeadline = ({ deadlineDetails }) => {
   return (
     <Card className="upcoming-deadline-card" style={{ height: '400px', overflowY: 'auto' }}>
       <Card.Body className="upcoming-deadline-card-body">
         <Card.Title className="upcoming-deadline-card-title">Upcoming Deadlines</Card.Title>
-        <Card.Text>
-          {[
-              ["Task 1", "Project 1", "10 days"],
-              ["Task 2", "Project 2", "2 days"],
-              ["Task 3", "Project 3", "1 day"],
-              ["Task 4", "Project 4", "1 day"],
-              ["Task 5", "Project 5", "1 day"],
-            ].map((deadlineDetail, key) => {
-              return (
-                <div key={key} className="upcoming-deadline-task">
-                  <div className="upcoming-deadline-task-details-top">
-                    <h4>{deadlineDetail[0]}</h4>
-                    <RxDot />
-                    {deadlineDetail[1]}
-                  </div>
-                  <div className="upcoming-deadline-task-details-bottom">
-                    <p>
-                      Remaining Time: <b>{deadlineDetail[2]}</b>
-                    </p>
-                  </div>
-                </div>
-              );
-            })}
-        </Card.Text>
+        <Card.Body> 
+          {
+            (deadlineDetails === null || deadlineDetails.length === 0) ? 
+            <p className="d-flex justify-content-center mt-3">No upcoming deadlines</p> :
+           (
+              deadlineDetails.map((deadlineDetail, key) => {
+                const taskDeadline = new Date(deadlineDetail.task.taskTimelineEndDateTime);
+                let currentDate = new Date();
+                currentDate.setTime(currentDate.getTime() + 480*60*1000);
+                currentDate = new Date(currentDate.toISOString().split("T")[0]);
+
+                if (taskDeadline > currentDate) {
+                  return (
+                    <div key={key} className="upcoming-deadline-task">
+                      <div className="upcoming-deadline-task-details-top">
+                        <h4>Project {deadlineDetail.projectName}</h4>
+                        <RxDot /> 
+                        Task {deadlineDetail.task.taskName}
+                      </div>
+                      <div className="upcoming-deadline-task-details-bottom">
+                        <p>
+                          Remaining Time: 
+                          <b>
+                            {' '}{ Math.ceil((taskDeadline - currentDate) / (1000 * 60 * 60 * 24)) }{" days"}  
+                          </b>
+                        </p>
+                      </div>
+                    </div>
+                  )
+                } else {
+                  return null;
+                }
+            }))
+          }
+        </Card.Body>
       </Card.Body>
     </Card>
   );
 };
+
+UpcomingDeadline.propTypes = {
+  deadlineDetails: PropTypes.array
+}
 
 export default UpcomingDeadline;
